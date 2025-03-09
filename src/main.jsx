92f884f5b9eaf119242c0104a328c11a4c675528
Dani Marcé Garcia <daniel.marce@mataro.epiaedu.cat>
2025-03-09 23:02:30 +0100

    feat: add sanitize.css and modify styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
+import "./sanitize.css";
 import "./index.css";
 import "./globals.css";
 
